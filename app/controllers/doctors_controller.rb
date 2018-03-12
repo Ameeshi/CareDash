be20f047e243b344aece49bd4f47b221ea078ba6
Ameeshi Goel <ameeshig@andrew.cmu.edu>
2018-03-11 22:57:55 -0400

    Fixed bugs

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,7 +4,7 @@ class DoctorsController < ApplicationController
   # GET /doctors
   # GET /doctors.json
   def index
-    @doctors = Doctor.alphabetical.paginate(page: params[:page]).per_page(10)
+    @doctors = Doctor.all
   end
 
   # GET /doctors/1
